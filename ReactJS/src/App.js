37debb849c3aee3e8607cdb127a3ba0312b090ac
bro <gudgudgud714@naver.com>
2022-10-04 14:56:45 +0900

    Add component - ExpenseItem

--- ReactJS/src/App.js
+++ ReactJS/src/App.js
@@ -1,7 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem /> {/* JSX코드내에서 사용자 지정 컴포넌트 사용할때는 대문자로 시작*/}
     </div>
   );
 }
