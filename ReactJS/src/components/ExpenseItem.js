e16d1666d24de28bea5c4f9c1a1d333ee6161802
bro <gudgudgud714@naver.com>
2022-10-06 15:14:51 +0900

    ExpenseItem 컴포넌트 ExpenseDate 컴포넌트 추가하여 컴포넌트 분할

--- ReactJS/src/components/ExpenseItem.js
+++ ReactJS/src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
     return (
         <div className='expense-item'>  {/* JSX는 class => className으로 사용*/}
-            <div>{props.date.toDateString()}</div>
+            <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
