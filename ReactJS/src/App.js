b8c8695ff304bb1903142660452d119147709a8d
bro <gudgudgud714@naver.com>
2022-10-11 15:16:57 +0900

    Expenses 컴포넌트 추가

--- ReactJS/src/App.js
+++ ReactJS/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -30,27 +30,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      {/* JSX코드내에서 사용자 지정 컴포넌트 사용할때는 대문자로 시작*/}
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 }
