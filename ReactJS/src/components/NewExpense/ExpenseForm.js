85a446abaf5644667556ff8e69c20ce023d4519a
bro <gudgudgud714@naver.com>
2022-10-14 15:34:04 +0900

    한개 이상의 useState 다루기

--- ReactJS/src/components/NewExpense/ExpenseForm.js
+++ ReactJS/src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,48 @@
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: '',
+    });
+
+    const titleChangeHandler = (e) => {
+        setUserInput({
+            ...userInput,
+            enteredTitle: e.target.value,
+        });
+    };
+
+    const amountChageHandler = (e) => {
+        setUserInput({
+            ...userInput,
+            enteredAmount: e.target.value,
+        });
+    };
+
+    const dateChageHandler = (e) => {
+        setUserInput({
+            ...userInput,
+            enteredDate: e.target.value,
+        });
+    };
+
     return (
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type='text' />
+                    <input type='text' onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type='number' min="0.01" step="0.01" />
+                    <input type='number' min="0.01" step="0.01" onChange={amountChageHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type='date' min="2022-01-01" max="2022-12-31" />
+                    <input type='date' min="2022-01-01" max="2022-12-31" onChange={dateChageHandler} />
                 </div>
             </div>
             <div className="new-expense__actions">
