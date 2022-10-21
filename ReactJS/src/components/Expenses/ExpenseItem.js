9bd606ef107f793bdf803059aaff3bb1e0a3a1cc
bro <gudgudgud714@naver.com>
2022-10-21 14:25:08 +0900

    사용하지 않는 useState 삭제 및 ExpenseItem 목록으로 랜더링

--- ReactJS/src/components/Expenses/ExpenseItem.js
+++ ReactJS/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -6,13 +6,15 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
     return (
-        <Card className='expense-item'>  {/* JSX는 class => className으로 사용*/}
-            <ExpenseDate date={props.date} />
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-        </Card>
+        <li>
+            <Card className='expense-item'>  {/* JSX는 class => className으로 사용*/}
+                <ExpenseDate date={props.date} />
+                <div className='expense-item__description'>
+                    <h2>{props.title}</h2>
+                    <div className='expense-item__price'>${props.amount}</div>
+                </div>
+            </Card>
+        </li>
     );
 }
 
