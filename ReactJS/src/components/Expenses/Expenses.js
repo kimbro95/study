71a3c576a43995431fc270d9a112f1ecf6ff03e2
bro <gudgudgud714@naver.com>
2022-10-21 14:33:30 +0900

    ExpensesList 컴포넌트 추가

--- ReactJS/src/components/Expenses/Expenses.js
+++ ReactJS/src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -15,25 +15,13 @@ const Expenses = (props) => {
         return item.date.getFullYear().toString() === filteredYear;
     });
 
-    let expensesContent = <p>No Expenses Found.</p>;
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((item) => (
-            <ExpenseItem
-                key={item.id}
-                title={item.title}
-                amount={item.amount}
-                date={item.date}
-            />
-        ));
-    }
-
     return (
         <Card className="expenses">{/* 사용자 지정 컴포넌트에 className을 쓸 경우 해당 컴포넌트에서 props.className로 접근 할 수 있다.*/}
             <ExpensesFilter
                 selected={filteredYear}
                 onChangeFilterYear={filterChangeHandler}
             />
-            {expensesContent}
+            <ExpensesList items={filteredExpenses} />
         </Card>
     );
 }
