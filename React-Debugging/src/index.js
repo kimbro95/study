8be48ae88acf9f5229686838d6594a7f06275182
bro <gudgudgud714@naver.com>
2022-11-02 11:05:41 +0900

    오류해결2 - React 18 버전에 맞게 수정 ReactDOM => createRoot

--- React-Debugging/src/index.js
+++ React-Debugging/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
