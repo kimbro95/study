d260a29c171e34f340c11ca85cb0a675dd22b7cd
bro <gudgudgud714@naver.com>
2022-11-03 15:47:37 +0900

    입력되는 값의 state 관리하기

--- ReactJS2/src/components/Users/AddUser.js
+++ ReactJS2/src/components/Users/AddUser.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import styles from './AddUser.module.css';
 
 const AddUser = () => {
+    const [enteredUserName, setEnteredUserName] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (e) => {
         e.preventDefault();
     };
+
+    const userNameChangeHandler = (e) => {
+        setEnteredUserName(e.target.value);
+    }
+
+    const ageChangeHandler = (e) => {
+        setEnteredAge(e.target.value);
+    }
 
     return (
         <Card cssClass={styles.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="userName">UserName</label>
-                <input id="userName" type="text" />
+                <input id="userName" type="text" value={enteredUserName} onChange={userNameChangeHandler} />
                 <label htmlFor="age">Age</label>
-                <input id="age" type="number" />
+                <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
                 <Button type="submit">Add</Button>
             </form>
         </Card>
