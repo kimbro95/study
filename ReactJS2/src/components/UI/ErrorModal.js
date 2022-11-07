2f3bb3430509fee9bfa09840083fc793044beca4
bro <gudgudgud714@naver.com>
2022-11-07 14:51:33 +0900

    React Portal을 이용한 Modal 구현

--- ReactJS2/src/components/UI/ErrorModal.js
+++ ReactJS2/src/components/UI/ErrorModal.js
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 
 import Card from './Card';
 import Button from './Button';
 import styles from './ErrorModal.module.css';
 
+const Backdrop = (props) => {
+    return <div className={styles.backdrop} onClick={props.onErrorHandler} />;
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <Card cssClass={styles.modal}>
+            <header className={styles.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={styles.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={styles.actions}>
+                <Button onClick={props.onErrorHandler}>Okay</Button>
+            </footer>
+        </Card>
+    );
+};
+
 const ErrorModal = (props) => {
     return (
-        <div>
-            <div className={styles.backdrop} onClick={props.onErrorHandler} />
-            <Card cssClass={styles.modal}>
-                <header className={styles.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={styles.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={styles.actions}>
-                    <Button onClick={props.onErrorHandler}>Okay</Button>
-                </footer>
-            </Card>
-        </div>
+        <Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onErrorHandler={props.onErrorHandler} />,
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    title={props.title}
+                    message={props.message}
+                    onErrorHandler={props.onErrorHandler}
+                />,
+                document.getElementById('modal-root')
+            )}
+        </Fragment>
     );
 }
 
