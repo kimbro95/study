90007d8245e247bca04aa823d8051175bc05d13f
bro <gudgudgud714@naver.com>
2022-11-18 16:25:31 +0900

    HeaderCartButton 컴포넌트 추가

--- ReactJS4/src/components/Layout/Header.js
+++ ReactJS4/src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React, { Fragment } from "react";
 
 import headerImg from '../../asseets/meals.jpg';
 import styles from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
     return (
         <Fragment>
             <header className={styles.header}>
                 <h1>Meals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={styles['main-image']}>
                 <img src={headerImg} alt="Header Meals" />
