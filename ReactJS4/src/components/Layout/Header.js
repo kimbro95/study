d03fe3212a1cbdae6576e586e0e3b4e69c5e0ff4
bro <gudgudgud714@naver.com>
2022-11-18 16:03:59 +0900

    Header 컴포넌트 추가

---
+++ ReactJS4/src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+
+import headerImg from '../../asseets/meals.jpg';
+import styles from './Header.module.css';
+
+const Header = () => {
+    return (
+        <Fragment>
+            <header className={styles.header}>
+                <h1>Meals</h1>
+                <button>Cart</button>
+            </header>
+            <div className={styles['main-image']}>
+                <img src={headerImg} alt="Header Meals Image" />
+            </div>
+        </Fragment>
+    );
+};
+
+export default Header;
