a9e40c171aea64cd743740d8dcd6b051351cdb3a
bro <gudgudgud714@naver.com>
2022-11-22 15:03:22 +0900

    Modal 컴포넌트 추가 및 index.html에 createPortal dom 추가

---
+++ ReactJS4/src/components/UI/Modal.js
@@ -0,0 +1,27 @@
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+import styles from './Modal.module.css';
+
+const Backdrop = (props) => {
+    return <div className={styles.backdrop} />;
+};
+const ModalOverlay = (props) => {
+    return (
+        <div className={styles.modal}>
+            <div>{props.children}</div>
+        </div>
+    );
+};
+
+const portalElement = document.getElementById("overlays");
+const Modal = (props) => {
+    return (
+        <Fragment>
+            {ReactDOM.createPortal(<Backdrop />, portalElement)}
+            {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+        </Fragment>
+    );
+};
+
+export default Modal;
