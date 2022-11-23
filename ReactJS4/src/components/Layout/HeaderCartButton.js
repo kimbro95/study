195852d064fcf2a65030dd4d865a01157ea3e365
bro <gudgudgud714@naver.com>
2022-11-23 14:58:06 +0900

    CartContext 컨텍스트 값 사용하기

--- ReactJS4/src/components/Layout/HeaderCartButton.js
+++ ReactJS4/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
+import React, { useContext } from "react";
+
 import CartIcon from "../Cart/CartIcon";
 import styles from './HeaderCartButton.module.css';
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+    const countCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
     return (
         <button className={styles.button} onClick={props.onClick}>
             <span className={styles.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={styles.badge}>2</span>
+            <span className={styles.badge}>{countCartItems}</span>
         </button>
     );
 };
