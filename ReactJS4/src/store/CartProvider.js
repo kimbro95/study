afc55fb41f0e7b9b004bae3e5cb61deaae53b014
bro <gudgudgud714@naver.com>
2022-11-28 16:25:15 +0900

    장바구니에 같은 item을 담을 경우 amount만 증가하도록 로직 수정

--- ReactJS4/src/store/CartProvider.js
+++ ReactJS4/src/store/CartProvider.js
@@ -8,8 +8,23 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if (action.type === 'ADD_CART_ITEM') {
-        const updateItems = state.items.concat(action.item);
         const updateTotalAmount = state.totalAmount + (action.item.price * action.item.amount);
+        const existingCartItemIndex = state.items.findIndex((item) => (
+            item.id === action.item.id
+        ));
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        let updateItems;
+        if (existingCartItem) {
+            const updateItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+            updateItems = [...state.items];
+            updateItems[existingCartItemIndex] = updateItem;
+        } else {
+            updateItems = state.items.concat(action.item);
+        }
         return { items: updateItems, totalAmount: updateTotalAmount };
     }
     return defaultCartState;
