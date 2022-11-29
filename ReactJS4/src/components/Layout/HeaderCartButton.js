e6c516795ebf3314fa65f92dd6e8d18f2d56ac5a
bro <gudgudgud714@naver.com>
2022-11-29 15:15:42 +0900

    useEffect훅을 이용하여 HeaderCartButton 애니메이션 효과

--- ReactJS4/src/components/Layout/HeaderCartButton.js
+++ ReactJS4/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import styles from './HeaderCartButton.module.css';
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+    const [btnIsBump, setBtnIsBump] = useState(false);
     const cartCtx = useContext(CartContext);
-    const countCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const { items } = cartCtx;
+
+    const countCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
 
+    const btnStyles = `${styles.button} ${btnIsBump ? styles.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length > 0) {
+            setBtnIsBump(true);
+        }
+
+        const cartTimer = setTimeout(() => {
+            setBtnIsBump(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(cartTimer);
+        };
+    }, [items]);
+
     return (
-        <button className={styles.button} onClick={props.onClick}>
+        <button className={btnStyles} onClick={props.onClick}>
             <span className={styles.icon}>
                 <CartIcon />
             </span>
