5f22512274b240a56c87b948c174165e46a378be
bro <gudgudgud714@naver.com>
2022-12-08 14:32:19 +0900

    submit 시 useRef와 useState를 이용하는 방법

--- ReactJS6/src/components/SimpleInput.js
+++ ReactJS6/src/components/SimpleInput.js
@@ -1,9 +1,37 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef();
+
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    // useState 방식
+    console.log(enteredName);
+
+    // uesRef 방식
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    // nameInputRef.current.value = '';  //직접적으로 DOM을 작동하는 방식은 X
+    setEnteredName('');
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          ref={nameInputRef}
+          type='text'
+          id='name'
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
