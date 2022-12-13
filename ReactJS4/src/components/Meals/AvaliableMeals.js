ec4373ca6aa7ce7f796f8e8d8afe4a67659a4d5c
bro <gudgudgud714@naver.com>
2022-12-13 16:27:02 +0900

    더미데이터 >>> 파이어베이스 데이터 http GET 요청으로 변경

--- ReactJS4/src/components/Meals/AvaliableMeals.js
+++ ReactJS4/src/components/Meals/AvaliableMeals.js
@@ -1,49 +1,51 @@
 import styles from './AvaliableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
-
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+import useFetch from '../../hooks/useFetch';
+import { useEffect, useState } from 'react';
 
 const AvaliableMeals = () => {
+    const [meals, setMeals] = useState([]);
+    const { isLoading, error, sendRequest: fetchMeals } = useFetch();
+
+    const mealList = (data) => {
+        const loadedMeals = [];
+        for (const key in data) {
+            loadedMeals.push({
+                id: key,
+                name: data[key].name,
+                price: data[key].price,
+                description: data[key].description,
+            });
+        }
+        setMeals(loadedMeals);
+    }
+    useEffect(() => {
+        fetchMeals(
+            { url: 'https://react-http-ff0f3-default-rtdb.asia-southeast1.firebasedatabase.app/meals' },
+            mealList
+        );
+    }, [fetchMeals]);
+
     return (
         <section className={styles.meals}>
             <Card>
-                <ul>
-                    {DUMMY_MEALS.map((meal) =>
-                        <MealItem
-                            id={meal.id}
-                            key={meal.id}
-                            name={meal.name}
-                            price={meal.price}
-                            description={meal.description}
-                        />
-                    )}
-                </ul>
+                {isLoading && <p>Loading...</p>}
+                {!isLoading && meals.length === 0 && !error && <p>Found No Meals.</p>}
+                {!isLoading && meals.length > 0 &&
+                    <ul>
+                        {meals.map((meal) =>
+                            <MealItem
+                                id={meal.id}
+                                key={meal.id}
+                                name={meal.name}
+                                price={meal.price}
+                                description={meal.description}
+                            />
+                        )}
+                    </ul>
+                }
+                {!isLoading && error && <p>{error}</p>}
             </Card>
         </section>
     );
