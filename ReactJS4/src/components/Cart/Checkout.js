a8e6becf28642720797b5f040077df2c0bbb4a8b
bro <gudgudgud714@naver.com>
2022-12-15 14:53:27 +0900

    Checkout 컴포넌트 유효성 검증 추가

--- ReactJS4/src/components/Cart/Checkout.js
+++ ReactJS4/src/components/Cart/Checkout.js
@@ -1,23 +1,71 @@
+import { useRef, useState } from 'react';
+
 import styles from './Checkout.module.css';
 
+const isEmpty = (value) => value.trim() === '';
+const isFiveChars = (value) => value.length !== 5;
+
 const Checkout = (props) => {
+    const [formInputValidity, setFormInputValidity] = useState({
+        name: true,
+        street: true,
+        postalCode: true,
+    });
+    const nameInputRef = useRef();
+    const streetInputRef = useRef();
+    const postalCodeInputRef = useRef();
+
     const submitHandler = (e) => {
         e.preventDefault();
 
+        const enteredName = nameInputRef.current.value;
+        const enteredStreet = streetInputRef.current.value;
+        const enteredPostalCode = postalCodeInputRef.current.value;
+
+        const enteredNameIsValid = !isEmpty(enteredName);
+        const enteredStreetIsValid = !isEmpty(enteredStreet);
+        const enteredPostalCodeIsValid = !isFiveChars(enteredPostalCode);
+
+        setFormInputValidity({
+            name: enteredNameIsValid,
+            street: enteredStreetIsValid,
+            postalCode: enteredPostalCodeIsValid,
+        });
+
+        const formIsValid = enteredNameIsValid && enteredStreetIsValid && enteredPostalCodeIsValid;
+        if (!formIsValid) {
+            return;
+        }
     };
+
     return (
         <form className={styles.form} onSubmit={submitHandler}>
-            <div className={styles.control}>
+            <div className={`${styles.control} ${formInputValidity.name ? '' : styles.invalid}`}>
                 <label htmlFor='name'>Your Name</label>
-                <input type='text' id='name' />
+                <input
+                    type='text'
+                    id='name'
+                    ref={nameInputRef}
+                />
+                {!formInputValidity.name && <p>Pls enter a valid name</p>}
             </div>
-            <div className={styles.control}>
+            <div className={`${styles.control} ${formInputValidity.street ? '' : styles.invalid}`}>
                 <label htmlFor='street'>Street</label>
-                <input type='text' id='street' />
+                <input
+                    type='text'
+                    id='street'
+                    ref={streetInputRef}
+                />
+                {!formInputValidity.street && <p>Pls enter a valid street</p>}
             </div>
-            <div className={styles.control}>
+            <div className={`${styles.control} ${formInputValidity.postalCode ? '' : styles.invalid}`}>
                 <label htmlFor='postal'>Postal Code</label>
-                <input type='text' id='postal' />
+                <input
+                    type='text'
+                    id='postal'
+                    ref={postalCodeInputRef}
+                />
+                {!formInputValidity.postalCode && <p>Pls enter a valid postalCode ( 5 characters long )</p>}
             </div>
             <div className={styles.actions}>
                 <button type='button' onClick={props.onIsShowCart}>
