50f01d8cbb7280fdd8e7ae4079ec08fa6120a13b
bro <gudgudgud714@naver.com>
2023-01-04 15:12:16 +0900

    redux에서 여러 state 속성 사용하기

--- Redux/src/components/Counter.js
+++ Redux/src/components/Counter.js
@@ -5,6 +5,7 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const isShow = useSelector(state => state.isShow);
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
@@ -18,12 +19,14 @@ const Counter = () => {
     dispatch({ type: 'decrement' });
   };
 
-  const toggleCounterHandler = () => { };
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER {counter} --</div>
+      {isShow && <div className={classes.value}>-- COUNTER {counter} --</div>}
       <div>
         <button onClick={incrementHandler}>+</button>
         <button onClick={increaseHandler}>+5</button>
--- Redux/src/store/index.js
+++ Redux/src/store/index.js
@@ -1,14 +1,32 @@
 import { createStore, } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, isShow: true };
+
+const counterReducer = (state = initialState, action) => {
     if (action.type === 'increment') {
-        return { counter: state.counter + 1 };
+        return {
+            counter: state.counter + 1,
+            isShow: state.isShow,
+        };
     }
     if (action.type === 'increase') {
-        return { counter: state.counter + action.value };
+        return {
+            counter: state.counter + action.value,
+            isShow: state.isShow,
+        };
     }
     if (action.type === 'decrement') {
-        return { counter: state.counter - 1 };
+        return {
+            counter: state.counter - 1,
+            isShow: state.isShow,
+        };
+    }
+
+    if (action.type === 'toggle') {
+        return {
+            counter: state.counter,
+            isShow: !state.isShow,
+        };
     }
     return state;
 };
