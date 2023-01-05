ab3f2773ebf25c54ad8c6fa110a475242689b787
bro <gudgudgud714@naver.com>
2023-01-05 14:42:55 +0900

    authSlice 데이터 읽기 및 dispatch 하기

--- Redux/src/App.js
+++ Redux/src/App.js
@@ -1,9 +1,21 @@
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 import Counter from './components/Counter';
 
-
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuth);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
--- Redux/src/components/Header.js
+++ Redux/src/components/Header.js
@@ -1,21 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { authActions } from '../store';
 import classes from './Header.module.css';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(state => state.auth.isAuth);
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
+        {isAuth &&
+          <ul>
+            <li>
+              <a href='/'>My Products</a>
+            </li>
+            <li>
+              <a href='/'>My Sales</a>
+            </li>
+
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        }
       </nav>
     </header>
   );
