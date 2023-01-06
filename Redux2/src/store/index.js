1499dc0819eaf04153092ec1d3dbd7eebea8b134
bro <gudgudgud714@naver.com>
2023-01-06 14:46:59 +0900

    Cart, CartButton, Products 컴포넌트 RTK 와 연결하기

--- Redux2/src/store/index.js
+++ Redux2/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import cartSlice from "./cart";
 import uiSlice from "./ui";
 
 const store = configureStore({
     reducer: {
         ui: uiSlice.reducer,
+        cart: cartSlice.reducer,
     }
 });
 
