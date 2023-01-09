382cc99f6e62778896670df889484d2b5ddcdc3d
bro <gudgudgud714@naver.com>
2023-01-09 15:42:21 +0900

    Thunk를 이용한 비동기 코드 처리하기

--- Redux2/src/App.js
+++ Redux2/src/App.js
@@ -1,7 +1,7 @@
 import { Fragment, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { uiActions } from './store/ui';
+import { sendCartData } from './store/cart';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -16,46 +16,12 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const notification = useSelector((state) => state.ui.notification);
 
-  /* 
-  redux slice의 reducers에서 비동기 fetch() 같은 처리를 해서는 안된다.
-  fetch를 사용할 경우 useEffect를 사용하여 처리한다.
-  */
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'sending',
-        title: 'Sending...',
-        message: 'Sending Cart Data...',
-      }));
-
-      const response = await fetch('https://study-a117e-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
-        method: 'PUT',
-        body: JSON.stringify(cart),
-      });
-
-      if (!response.ok) {
-        throw new Error('Sending Cart Data Failed !!!');
-      }
-
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success...',
-        message: 'Sending Cart Data Successfully !!!',
-      }));
-    }
-
     if (isInitial) {
       isInitial = false;
       return;
     }
-
-    sendCartData().catch(err => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error !!!',
-        message: 'Sending Cart Data Failed !!!',
-      }));
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
--- Redux2/src/store/cart.js
+++ Redux2/src/store/cart.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { uiActions } from "./ui";
+
 const initialState = { items: [], totalQuantity: 0 }
 
 const cartSlice = createSlice({
@@ -38,6 +40,52 @@ const cartSlice = createSlice({
         },
     }
 });
+
+/* 
+Thunk를 이용한 비동기 코드 처리하기
+Thunk - 다른 작업이 완료될때 까지 작업을 지연시키는 함수
+*/
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+        dispatch(
+            uiActions.showNotification({
+                status: 'pending',
+                title: 'Sending...',
+                message: 'Sending Cart Data...',
+            })
+        );
+
+        const sendRequest = async () => {
+            const response = await fetch('https://study-a117e-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
+                method: 'PUT',
+                body: JSON.stringify(cart),
+            });
+
+            if (!response.ok) {
+                throw new Error('Sending Cart Data Failed !!!');
+            }
+        };
+
+        try {
+            await sendRequest();
+            dispatch(
+                uiActions.showNotification({
+                    status: 'success',
+                    title: 'Success...',
+                    message: 'Sending Cart Data Successfully !!!',
+                })
+            );
+        } catch (e) {
+            dispatch(
+                uiActions.showNotification({
+                    status: 'error',
+                    title: 'Error !!!',
+                    message: 'Sending Cart Data Failed !!!',
+                })
+            );
+        }
+    };
+};
 
 export const cartActions = cartSlice.actions;
 
