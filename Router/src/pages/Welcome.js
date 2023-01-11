9ce0805c912fa6d1c926ac561537834d30a9b968
bro <gudgudgud714@naver.com>
2023-01-11 15:10:29 +0900

    중첩 경로 사용하기

--- Router/src/pages/Welcome.js
+++ Router/src/pages/Welcome.js
@@ -1,9 +1,14 @@
+import { Route } from "react-router-dom";
+
 const Welcome = () => {
     return (
-        <h1>
-            Welcome !!!
-        </h1>
-    )
-}
+        <section>
+            <h1>Welcome !!!</h1>
+            <Route path="/welcome/new">
+                <p>welcome New</p>
+            </Route>
+        </section>
+    );
+};
 
 export default Welcome;
