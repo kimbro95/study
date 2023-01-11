a40db3945e7a224e3bd7c4195385694441ea6a0c
bro <gudgudgud714@naver.com>
2023-01-11 14:58:33 +0900

    useParams를 이용하여 url params 추출하기

--- Router/src/pages/ProductDetail.js
+++ Router/src/pages/ProductDetail.js
@@ -1,7 +1,14 @@
+import { useParams } from "react-router-dom";
+
 const ProductDetail = () => {
+    const params = useParams();
+    /*
+    <Route path="/productDetail/:id">
+    이 경로에서 :id 형태로 작성된 params 값을 가져올 수 있다.
+    */
     return (
         <section>
-            <h1>Product Detail</h1>
+            <h1>Product Detail {params.id}</h1>
         </section>
     );
 };
