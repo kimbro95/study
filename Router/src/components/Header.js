54506141053f94108172def569035577353702ac
bro <gudgudgud714@naver.com>
2023-01-11 14:15:04 +0900

    NavLink 사용

--- Router/src/components/Header.js
+++ Router/src/components/Header.js
@@ -1,23 +1,27 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+import classes from './Header.module.css';
 
 const Header = () => {
     return (
-        <header>
+        <header className={classes.header}>
             <nav>
                 <ul>
                     {/* 
-                    a 태그를 사용하면 spa에 의미가 사라짐
-                    spa가 되도록 Link를 사용
+                    @5 버전
+                    NavLink는 활성 앵커 항목에 css 클래스를 설정한다.
+                    activeClassName에 prop을 추가하면된다.
+                    ex)활성 클래스가 있는 앵커 태그를 선택하는 
+                    .header a.active 선택자를 추가한다.
+                    
+                    @6 버전
+                    isActive
                     */}
                     <li>
-                        <a href="/products">products</a>
-                    </li>
-
-                    <li>
-                        <Link to="/welcome">welcome</Link>
+                        <NavLink activeClassName={classes.active} to="/welcome">welcome</NavLink>
                     </li>
                     <li>
-                        <Link to="/products">products</Link>
+                        <NavLink activeClassName={classes.active} to="/products">products</NavLink>
                     </li>
                 </ul>
             </nav>
