2151fa979fb34e62c2a3c2ed0e833478d30dd2c5
bro <gudgudgud714@naver.com>
2023-01-12 16:16:09 +0900

    중첩 경로 사용법

--- Router2/src/pages/QuoteDetail.js
+++ Router2/src/pages/QuoteDetail.js
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
-import { useParams } from "react-router-dom";
+import { Route, useParams } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
     const params = useParams();
@@ -8,6 +10,14 @@ const QuoteDetail = () => {
         <Fragment>
             <h1>QuoteDetail</h1>
             <p>{params.quoteId}</p>
+
+            {/* 2가지 중첩 경로 사용법 */}
+            <Route path={`/quotes/${params.quoteId}/comments`}>
+                <Comments />
+            </Route>
+            <Route path={`/quotes/:quoteId/comments`}>
+                <Comments />
+            </Route>
         </Fragment>
     );
 };
