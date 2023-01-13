5757264f618e6498ece8d81f1dafab77d8459bff
bro <gudgudgud714@naver.com>
2023-01-13 14:24:37 +0900

    page not found 처리

--- Router2/src/App.js
+++ Router2/src/App.js
@@ -4,6 +4,7 @@ import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -20,6 +21,9 @@ function App() {
         </Route>
         <Route path='/newQuote'>
           <NewQuote />
+        </Route>
+        <Route path="*">
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
