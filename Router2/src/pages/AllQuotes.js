d8b68fd551a2a1dc7e692dd6688369af142131c0
bro <gudgudgud714@naver.com>
2023-01-13 16:11:08 +0900

    useLocation을 사용하여 쿼리매개 변수로 정렬하기

--- Router2/src/pages/AllQuotes.js
+++ Router2/src/pages/AllQuotes.js
@@ -1,9 +1,11 @@
 import QuoteList from "../components/quotes/QuoteList";
 
 const DUMMY_QUOTES = [
-    { id: 'q1', author: 'Max', text: 'Learning React is fun !!!' },
-    { id: 'q2', author: 'Min', text: 'Learning JS is fun !!!' },
-    { id: 'q3', author: 'Top', text: 'Learning Front-end is fun !!!' }
+    { id: 'q1', author: 'Banilla', text: 'Learning React is fun !!!' },
+    { id: 'q2', author: 'Ajax', text: 'Learning JS is fun !!!' },
+    { id: 'q3', author: 'Console', text: 'Learning Front-end is fun !!!' },
+    { id: 'q4', author: 'Data', text: 'Learning Front-end is fun !!!' },
+    { id: 'q5', author: 'End', text: 'Learning Front-end is fun !!!' }
 ];
 
 const AllQuotes = () => {
