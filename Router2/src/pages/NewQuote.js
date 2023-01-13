dc41f67dff16f468640f47c49593cfa270579140
bro <gudgudgud714@naver.com>
2023-01-13 15:40:57 +0900

    useHistory를 이용한 페이지 이동

--- Router2/src/pages/NewQuote.js
+++ Router2/src/pages/NewQuote.js
@@ -1,8 +1,18 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+    const history = useHistory();
+
     const addQuoteHandler = (quoteData) => {
         console.log(quoteData);
+
+        /*
+        페이지 이동
+        push - back 버튼을 사용하여 원래 페이지로 돌아갈 수 있다.
+        replace - back 버튼을 사용하여 원래 페이지로 돌아갈 수 없다.
+        */
+        history.push('/quotes');
     };
 
     return (
