8e4f31c8cbb28be7d630ad78b82cfb6f11b15942
bro <gudgudgud714@naver.com>
2023-01-16 16:30:49 +0900

    http 사용하여 댓글 기능 추가하기

--- Router2/src/components/comments/Comments.js
+++ Router2/src/components/comments/Comments.js
@@ -1,15 +1,47 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
 
 import classes from './Comments.module.css';
+
 import NewCommentForm from './NewCommentForm';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import CommentsList from '../comments/CommentsList';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+  const { quoteId } = params;
+  const { sendRequest, status, data: loadedComments, error } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-  
+
+  const addedCommentHandler = useCallback(async () => {
+    await sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  let comments;
+  if (status === 'pending') {
+    comments = <div className='centered'>
+      <LoadingSpinner />
+    </div>;
+  }
+
+  if (status === 'completed' && (loadedComments && loadedComments.length > 0)) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+
+  if (status === 'completed' && (!loadedComments || loadedComments.length === 0)) {
+    comments = <p className='centered'>No Comments</p>;
+  }
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
@@ -18,8 +50,13 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment &&
+        <NewCommentForm
+          quoteId={quoteId}
+          onAddedComment={addedCommentHandler}
+        />
+      }
+      {comments}
     </section>
   );
 };
