857fb4404998f8eb3c20e2f8af90b6783923c7f3
bro <gudgudgud714@naver.com>
2023-01-16 14:19:41 +0900

    중첩 경로 활용하기

--- Router2/src/pages/QuoteDetail.js
+++ Router2/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Redirect, Route, useParams } from "react-router-dom";
+import { Link, Route, useParams } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -29,6 +29,11 @@ const QuoteDetail = () => {
                 author={quote.author}
                 text={quote.text}
             />
+            <Route path={`/quotes/${params.quoteId}`} exact>
+                <div className="centered">
+                    <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>Load comments</Link>
+                </div>
+            </Route>
             <Route path={`/quotes/${params.quoteId}/comments`}>
                 <Comments />
             </Route>
