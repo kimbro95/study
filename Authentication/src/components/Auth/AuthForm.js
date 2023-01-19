5fd0dd21c427e22d286e6882363b514660bfdd8f
bro <gudgudgud714@naver.com>
2023-01-19 16:13:15 +0900

    파이어베이스 인증을 통한 사용자 추가 및 로그인 처리, 결과(실패)피드백

--- Authentication/src/components/Auth/AuthForm.js
+++ Authentication/src/components/Auth/AuthForm.js
@@ -1,28 +1,102 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
+const API_KEY = process.env.REACT_APP_FIREBASE_KEY;
+
 const AuthForm = () => {
+  const navigate = useNavigate();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const enteredEamil = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    setIsLoading(true);
+    let url;
+    if (isLogin) { // 로그인 처리
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          email: enteredEamil,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      setIsLoading(false);
+      const data = await response.json();
+      if (!response.ok) {
+        let errorMessage = 'Login Failed';
+        if (data && data.error && data.error.message) {
+          errorMessage = data.error.message;
+          alert(errorMessage);
+          throw new Error(errorMessage);
+        }
+      }
+      console.log(data);
+      navigate('/');
+    } else {  // 회원가입 처리
+      try {
+        url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
+        const response = await fetch(url, {
+          method: 'POST',
+          body: JSON.stringify({
+            email: enteredEamil,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+
+        setIsLoading(false);
+        if (!response.ok) {
+          const data = await response.json();
+          let errorMessage = 'Authentication Failed';
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message;
+            alert(errorMessage);
+            throw new Error(errorMessage);
+          }
+        }
+        alert('Create Account');
+      } catch (e) {
+        throw new Error(e);
+      }
+    }
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required ref={passwordInputRef} />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Loading...</p>}
           <button
             type='button'
             className={classes.toggle}
