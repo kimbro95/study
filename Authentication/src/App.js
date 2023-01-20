c3e3bd1cde386bc66b222ec22dc0514be446e383
bro <gudgudgud714@naver.com>
2023-01-20 11:16:37 +0900

    Redux state값으로 route 설정하기

--- Authentication/src/App.js
+++ Authentication/src/App.js
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
@@ -6,12 +7,15 @@ import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
   return (
     <Layout>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/auth' element={<AuthPage />} />
-        <Route path='/profile' element={<UserProfile />} />
+        {!isLoggedIn && <Route path='/auth' element={<AuthPage />} />}
+        {isLoggedIn && <Route path='/profile' element={<UserProfile />} />}
+        <Route path="*" element={<Navigate replace to='/' />} />
       </Routes>
     </Layout>
   );
