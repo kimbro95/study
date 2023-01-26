96ff674840281a301a914c1105ee4e8123466ef5
bro <gudgudgud714@naver.com>
2023-01-26 15:20:33 +0900

    Link를 이용한 페이지 이동

--- nextjs1/pages/news/index.js
+++ nextjs1/pages/news/index.js
@@ -1,5 +1,20 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/one">NextJS li one</Link>
+        </li>
+        <li>
+          <Link href="/news/two">NextJS li two</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default NewsPage;
