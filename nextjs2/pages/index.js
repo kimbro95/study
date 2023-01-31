4034a2b78f1c4a5d603c75d78819fcb14fc3be85
bro <gudgudgud714@naver.com>
2023-01-31 13:33:47 +0900

    더미 데이터로 리스트 출력

--- nextjs2/pages/index.js
+++ nextjs2/pages/index.js
@@ -1,5 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "http://eqsquare.co.kr/assets/images/patent/patent2023-01-11_z43lVkCHb9qWYcD8.png",
+    address: "First Meetup Address",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "http://eqsquare.co.kr/assets/images/patent/patent2023-01-11_z43lVkCHb9qWYcD8.png",
+    address: "Second Meetup Address",
+    description: "This is a second meetup!",
+  },
+];
+
 const index = () => {
-  return <h1>index</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 
 export default index;
