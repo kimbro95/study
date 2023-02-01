8547b7adb9206d3194fc5670743320a5875b69e0
bro <gudgudgud714@naver.com>
2023-02-01 14:21:05 +0900

    MeetupDetail 컴포넌트 및 CSS 모듈 추가

--- nextjs2/pages/[meetupId]/index.js
+++ nextjs2/pages/[meetupId]/index.js
@@ -1,9 +1,14 @@
-import { useRouter } from "next/router";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const meetUp = () => {
-  const router = useRouter();
-
-  return <h1>{router.query.meetupId}</h1>;
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="http://eqsquare.co.kr/assets/images/patent/patent2023-01-11_z43lVkCHb9qWYcD8.png"
+      title="A First Meetup"
+      address="First Meetup Address"
+      description="This is a first meetup!"
+    />
+  );
 };
 
-export default meetUp;
+export default MeetupDetails;
