d1137fef17494fb90ca3c2a107becb0ebf66632b
bro <gudgudgud714@naver.com>
2023-02-02 15:22:36 +0900

    getStaticPaths

--- nextjs2/pages/[meetupId]/index.js
+++ nextjs2/pages/[meetupId]/index.js
@@ -10,5 +10,40 @@ const MeetupDetails = () => {
     />
   );
 };
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "http://eqsquare.co.kr/assets/images/patent/patent2023-01-11_z43lVkCHb9qWYcD8.png",
+        title: "A First Meetup",
+        address: "First Meetup Address",
+        description: "This is a first meetup!",
+      },
+    },
+  };
+};
 
 export default MeetupDetails;
