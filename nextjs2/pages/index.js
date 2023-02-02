720a5e6533d7a2b20db54cc797ccd06ea6b8aad1
bro <gudgudgud714@naver.com>
2023-02-02 15:02:45 +0900

    getServerSideProps를 사용하여 SSR 사용

--- nextjs2/pages/index.js
+++ nextjs2/pages/index.js
@@ -23,13 +23,28 @@ const index = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
 
-export const getStaticProps = async () => {
+/**
+ *  getServerSideProps
+ *  getStaticProps와 차이점 빌드 프로세스 중에는 실행되지 않음
+ */
+export const getServerSideProps = async (context) => {
+  const req = context.req;
+  const res = context.res;
+
   return {
     props: {
       meetups: DUMMY_MEETUPS,
     },
-    revalidate: 1, // 이 페이지에 요청이 들어오면 1초마다 서버에서 페이지를 다시 생성한다.
   };
 };
+
+// export const getStaticProps = async () => {
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//     revalidate: 1, // 이 페이지에 요청이 들어오면 1초마다 서버에서 페이지를 다시 생성한다.
+//   };
+// };
 
 export default index;
