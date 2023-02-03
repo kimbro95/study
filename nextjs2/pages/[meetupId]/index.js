552d5a6dedde5dee5732e00e792e4d2b2bc391a6
bro <gudgudgud714@naver.com>
2023-02-03 14:36:34 +0900

    getStaticProps에서 GET http 요청 보내기

--- nextjs2/pages/[meetupId]/index.js
+++ nextjs2/pages/[meetupId]/index.js
@@ -1,46 +1,48 @@
+import excuteQuery from "../../lib/dbConnect";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
+  const { image, title, address, description } = props.meetupData;
   return (
     <MeetupDetail
-      image="http://eqsquare.co.kr/assets/images/patent/patent2023-01-11_z43lVkCHb9qWYcD8.png"
-      title="A First Meetup"
-      address="First Meetup Address"
-      description="This is a first meetup!"
+      image={image}
+      title={title}
+      address={address}
+      description={description}
     />
   );
 };
+
 export const getStaticPaths = async () => {
+  const meetups = await excuteQuery({
+    query: "SELECT idx FROM meetups",
+    values: [],
+  });
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup.idx.toString() },
+    })),
   };
 };
 
 export const getStaticProps = async (context) => {
   const meetupId = context.params.meetupId;
+  const selectedMeetup = await excuteQuery({
+    query: "SELECT * FROM meetups WHERE idx = ?",
+    values: [meetupId],
+  });
+  const result = selectedMeetup[0];
 
-  console.log(meetupId);
   return {
     props: {
       meetupData: {
-        id: meetupId,
-        image:
-          "http://eqsquare.co.kr/assets/images/patent/patent2023-01-11_z43lVkCHb9qWYcD8.png",
-        title: "A First Meetup",
-        address: "First Meetup Address",
-        description: "This is a first meetup!",
+        id: result.idx,
+        image: result.image,
+        title: result.title,
+        address: result.address,
+        description: result.description,
       },
     },
   };
--- nextjs2/pages/index.js
+++ nextjs2/pages/index.js
@@ -1,23 +1,5 @@
+import excuteQuery from "../lib/dbConnect";
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image:
-      "http://eqsquare.co.kr/assets/images/patent/patent2023-01-11_z43lVkCHb9qWYcD8.png",
-    address: "First Meetup Address",
-    description: "This is a first meetup!",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image:
-      "http://eqsquare.co.kr/assets/images/patent/patent2023-01-11_z43lVkCHb9qWYcD8.png",
-    address: "Second Meetup Address",
-    description: "This is a second meetup!",
-  },
-];
 
 const index = (props) => {
   return <MeetupList meetups={props.meetups} />;
@@ -27,24 +9,35 @@ const index = (props) => {
  *  getServerSideProps
  *  getStaticProps와 차이점 빌드 프로세스 중에는 실행되지 않음
  */
-export const getServerSideProps = async (context) => {
-  const req = context.req;
-  const res = context.res;
+// export const getServerSideProps = async (context) => {
+//   const req = context.req;
+//   const res = context.res;
 
-  return {
-    props: {
-      meetups: DUMMY_MEETUPS,
-    },
-  };
-};
-
-// export const getStaticProps = async () => {
 //   return {
 //     props: {
 //       meetups: DUMMY_MEETUPS,
 //     },
-//     revalidate: 1, // 이 페이지에 요청이 들어오면 1초마다 서버에서 페이지를 다시 생성한다.
 //   };
 // };
+
+export const getStaticProps = async () => {
+  const meetups = await excuteQuery({
+    query: "SELECT * FROM meetups",
+    values: [],
+  });
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        id: meetup.idx,
+        title: meetup.title,
+        image: meetup.image,
+        address: meetup.address,
+        description: meetup.description,
+      })),
+    },
+    revalidate: 1, // 이 페이지에 요청이 들어오면 1초마다 서버에서 페이지를 다시 생성한다.
+  };
+};
 
 export default index;
